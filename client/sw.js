ae194c2a0a802258a2e9863474818332a6f4add9
mohammed <mohammedbchaiker@gmail.com>
2022-09-20 10:30:19 +0200

    commit n4

--- client/sw.js
+++ client/sw.js
@@ -2,6 +2,7 @@ self.addEventListener('push', (e) => {
   const data = e.data.json();
   self.registration.showNotification(data.title, {
     body: data.body,
-    icon: data.icon,
+icon:'https://png.pngtree.com/png-vector/20191021/ourlarge/pngtree-vector-cycling-icon-png-image_1834570.jpg',
+image: 'https://png.pngtree.com/png-vector/20191021/ourlarge/pngtree-vector-cycling-icon-png-image_1834570.jpg'
   });
 });
